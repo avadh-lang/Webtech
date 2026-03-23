0f55bd3ffa2a34b48062a127d64d8932a477e230
AVADH MEHTA <avadhmehta@AVADHs-MacBook-Pro.local>
2026-03-23 18:16:33 +0530

    Added Exp3 - Landing Page

---
+++ exp3/script.js
@@ -0,0 +1,23 @@
+// Button click
+function showMessage() {
+    alert("lessgooo 🚀");
+}
+
+// Feature click
+function featureClick(feature) {
+    alert(feature + " selected!");
+}
+
+// Form validation
+function validateForm() {
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+
+    if (name === "" || email === "") {
+        alert("Please fill all fields!");
+        return false;
+    }
+
+    alert("Form submitted successfully!");
+    return true;
+}
